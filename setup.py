906bf3a9d6a24da179968cff45f63cff656d5548
raveneyes <christian.c.anderson@gmail.com>
2016-01-24 17:13:38 -0500

    add setup.py and setup.cfg

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pygameday',
+  packages = ['pygameday'],
+  version = '0.1',
+  description = 'Module for scraping, parsing, and ingesting MLB GameDay data into a database',
+  author = 'Chris Anderson',
+  author_email = 'christian.c.anderson@gmail.com',
+  url = 'https://github.com/raveneyes/pygameday',
+  download_url = 'https://github.com/raveneyes/pygameday/tarball/0.1',
+  keywords = ['baseball', 'gameday', 'database', 'scraping'],
+  classifiers = [],
+)
