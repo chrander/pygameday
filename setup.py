fe5b42d07e4c35c850a581b0813dc1481e8e6fa9
Chris Anderson <christian.c.anderson@gmail.com>
2016-05-16 11:22:12 -0400

    Update URLs in setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
   description = 'Module for scraping, parsing, and ingesting MLB GameDay data into a database',
   author = 'Chris Anderson',
   author_email = 'christian.c.anderson@gmail.com',
-  url = 'https://github.com/raveneyes/pygameday',
-  download_url = 'https://github.com/raveneyes/pygameday/tarball/0.1',
+  url = 'https://github.com/chrander/pygameday',
+  download_url = 'https://github.com/chrander/pygameday/tarball/0.1',
   keywords = ['baseball', 'gameday', 'database', 'scraping'],
   classifiers = [],
 )
