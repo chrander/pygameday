ee7c02f7d85002dc1f4208c06a077a29518866ce
Chris Anderson <christian.c.anderson@gmail.com>
2018-05-08 00:24:37 -0400

    Refactor main scripts

---
+++ scripts/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from datetime import datetime
+from pygameday import GameDayClient
+
+try:
+    # Look for Database URI in config_mine.py first.
+    # config_mine.py should not be version controlled, because it can contain username/password information.
+    # config.py, a default config file, is version controlled. It should not have any usernames, passwords, or other
+    # potentially sensitive information.
+    from config_mine import DATABASE_URI
+except ImportError:
+    from config import DATABASE_URI
+
+
+start_date = datetime(2018, 3, 31)
+end_date = datetime(2018, 3, 31)
+
+client = GameDayClient(DATABASE_URI)
+client.process_date_range(start_date, end_date)
