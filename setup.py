42eb8f109eac1cbcea64fab35b3a5121c4f4096b
Chris Anderson <christian.c.anderson@gmail.com>
2018-05-08 11:14:03 -0400

    add install_requires to setup

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 from distutils.core import setup
 
 setup(
-  name = 'pygameday',
-  packages = ['pygameday'],
-  version = '0.2',
-  description = 'Module for scraping, parsing, and ingesting MLB GameDay data into a database',
-  author = 'Chris Anderson',
-  author_email = 'christian.c.anderson@gmail.com',
-  url = 'https://github.com/chrander/pygameday',
-  download_url = 'https://github.com/chrander/pygameday/tarball/0.1',
-  keywords = ['baseball', 'gameday', 'database', 'scraping'],
-  classifiers = [],
+    name = 'pygameday',
+    version = '0.2',
+    author = 'Chris Anderson',
+    author_email = 'christian.c.anderson@gmail.com',
+    packages = ['pygameday'],
+    url = 'https://github.com/chrander/pygameday',
+    license='LICENSE.txt',
+    description = 'Module for scraping, parsing, and ingesting MLB GameDay data into a database',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'python-dateutil',
+        'sqlalchemy',
+
+    ],
+    download_url = 'https://github.com/chrander/pygameday/tarball/0.1',
+    keywords = ['baseball', 'gameday', 'database', 'scraping'],
+    classifiers = [],
 )
