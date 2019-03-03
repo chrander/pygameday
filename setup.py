2fcde65de436ace5424ff2eb5893c6eb5cd07002
Chris Anderson <christian.c.anderson@gmail.com>
2019-03-03 00:39:50 -0500

    update setup.py to conform better to PyPi standards

--- setup.py
+++ setup.py
@@ -1,21 +1,33 @@
-from distutils.core import setup
+# from distutils.core import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
     name = 'pygameday',
-    version = '0.3',
+    version = '0.3.4',
     author = 'Chris Anderson',
     author_email = 'christian.c.anderson@gmail.com',
-    packages = ['pygameday'],
+    packages = setuptools.find_packages(),
     url = 'https://github.com/chrander/pygameday',
     license='LICENSE.txt',
     description = 'Module for scraping, parsing, and ingesting MLB GameDay data into a database',
-    long_description=open('README.md').read(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=[
-        'python-dateutil',
+        'tqdm',
         'sqlalchemy',
-        'tqdm'
+        'python-dateutil',
+        'requests',
+        'lxml'
     ],
-    download_url = 'https://github.com/chrander/pygameday/tarball/0.3',
+    download_url = 'https://github.com/chrander/pygameday/tarball/0.3.4',
     keywords = ['baseball', 'gameday', 'database', 'scraping'],
-    classifiers = [],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
